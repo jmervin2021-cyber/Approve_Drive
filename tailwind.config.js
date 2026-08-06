0992e32f218e699410cc23ae4f110d13186f6e14
jmervin2021-cyber <jmervin2021@gmail.com>
2026-08-06 17:09:27 +0000

    u10

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,24 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  extend: {
-    colors: {
-      crews: {
-        asphalt: '#111827',
-        charcoal: '#1F2937',
-        slate: '#374151',
-        orange: '#F97316',
-        yellow: '#EAB308',
-        green: '#22C55E',
-        red: '#EF4444',
+    extend: {
+      colors: {
+        crews: {
+          asphalt: '#111827',
+          charcoal: '#1F2937',
+          slate: '#374151',
+          orange: '#F97316',
+          yellow: '#EAB308',
+          green: '#22C55E',
+          red: '#EF4444',
+        }
       }
-    }
+    },
   },
-},
+  plugins: [],
+};
